19a184dc5746f640723273aa08e7f3b154d91266
Pavel Medvedev <pmedvedev@gmail.com>
2013-09-20 18:03:10 +0400

    Moving image module from v8-juice to v8pp

--- src/image.cpp
+++ src/image.cpp
@@ -2,136 +2,32 @@
 #include "library.hpp"
 
 using namespace v8;
-using namespace v8::juice;
-using namespace v8::juice::convert;
-using namespace aspect;
-using namespace aspect::v8_core;
-
-// DECLARE_LIBRARY_ENTRYPOINTS(image_install, image_uninstall);
-
-V8_IMPLEMENT_CLASS_BINDER(aspect::image::device, device);
-
-/*
-
-TODO - invoke from dependent libraries
-set global "initialized" state - init only once
-destroy in dependent libraries
-maintain reference count.
-
-do this via shared pointer?  so that when 0 references the object is destroyed?
-
-aspect::image::init(target);
-aspect::image::cleanup();
-
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  INSTALL IMAGE LIBRARY INTO v8 GLOBAL OBJECT AUTOMATICALLY
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  THIS WAY WE DO NOT NEED KEEP/PASS TARGET!
-
-
-class local_initializer
-{
-	public:
-
-		local_initializer();
-		~local_initializer();
-};
-
-static local_initializer g_local_initializer;
-*/
-
-void image_install(Handle<Object> target)
-{
-
-	// https://github.com/ofTheo/videoInput/blob/master/videoInputSrcAndDemos/libs/videoInput/videoInput.cpp
-
-	// http://www.codeproject.com/Articles/12869/Real-time-video-image-processing-frame-grabber-usi
-
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	using namespace aspect::image;
-
-	ClassBinder<device>* binder = new ClassBinder<device>(target);
-	V8_SET_CLASS_BINDER(device, binder);
-	(*binder)
-		.BindMemFunc<uint32_t, &device::get_dropped_frames>("get_dropped_frames")
-		.Seal();
-
-
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-	// MOVE THIS TO DLLMAIN!
-
-}
-
-void image_uninstall(Handle<Object> target) 
-{
-	V8_DESTROY_CLASS_BINDER(aspect::image::device);
-}
-
-namespace v8 { namespace juice {
-
-	V8_DECLARE_WEAK_CLASS_CTOR(aspect::image::device, args, exceptionText)
-	{
-		throw std::runtime_error("only derived classes may be created");
-	}
-
-	V8_DECLARE_WEAK_CLASS_DTOR(aspect::image::device, o)
-	{
-		o->release();
-	}
-} } // v8::juice
 
 namespace aspect { namespace image {
 
-uint32_t g_reference_count = 0;
+// https://github.com/ofTheo/videoInput/blob/master/videoInputSrcAndDemos/libs/videoInput/videoInput.cpp
+// http://www.codeproject.com/Articles/12869/Real-time-video-image-processing-frame-grabber-usi
 
-void init(void)
+using namespace v8;
+
+DECLARE_LIBRARY_ENTRYPOINTS(image_install, image_uninstall);
+
+Handle<Value> image_install()
 {
-	using namespace aspect::image;
+	v8pp::class_<device, v8pp::no_factory>device_class;
+	device_class
+		.set("get_dropped_frames", &device::get_dropped_frames)
+		;
 
-	g_reference_count++;
-	if(g_reference_count > 1)
-		return;
-
-	Handle<Object> target = v8::Context::GetCurrent()->Global();
-
-	ClassBinder<device>* binder = new ClassBinder<device>(target);
-	V8_SET_CLASS_BINDER(device, binder);
-	(*binder)
-		.BindMemFunc<uint32_t, &device::get_dropped_frames>("get_dropped_frames")
-		.Seal();
+	v8pp::module image_module;
+	image_module.set("__image_device_interface", device_class);
+	return image_module.new_instance();
 }
 
-void cleanup(void)
+void image_uninstall(Handle<Value> library)
 {
-	g_reference_count--;
-	if(g_reference_count)
-		return;
-
-//	Handle<Object> target = v8::Context::GetCurrent()->Global();
-	V8_DESTROY_CLASS_BINDER(device);
 }
 
-std::vector<boost::shared_ptr<device>>	g_devices;
 uint64_t bitmap::total_memory_ = 0;
 
 enum grid_type { GRID_LINE, GRID_1, GRID_2 };
@@ -173,76 +69,34 @@ void bitmap::checker2(uint32_t c1, uint32_t c2)
 	}
 }
 
-	// ----------------
-/*
-	void image_device_bindings(binding_list &list, Handle<Object> global)
-	{
-		ClassBinder<device>* binder = new ClassBinder<device>(global);
-		list.push_back(binder);
-		V8_SET_CLASS_BINDER(device, binder);
-		(*binder)
-			.BindMemFunc<uint32_t, &device::get_dropped_frames>("get_dropped_frames")
-			.Seal();
-
-		// 	binder_device = new ClassBinder<device>(target);
-		// 	V8_SET_CLASS_BINDER(device, binder_device);
-		// 	(*binder_device)
-		// 		.Seal();
-
-	}
-*/
-//	aspect::v8_core::register_v8_binding image_device_bindings_init(&aspect::image::image_device_bindings);
-
-
-void device::schedule_input_frame( boost::shared_ptr<shared_bitmap_container> & frame, bool drop_frames)
+void device::schedule_input_frame(shared_bitmap_container const& frame, bool drop_frames)
 {
-
 	capture_queue_.push(frame);
 
 	// clear up the queue so that at most we have 2 pending frames
-	if(drop_frames)
+	if (drop_frames)
 	{
-		while(capture_queue_.size() > 2)
+		while (capture_queue_.size() > 2)
 		{
-			if(trace_dropped_frames_)
-				printf("image::device \"%s\" dropping input frame\n",name_.c_str());
-			boost::shared_ptr<shared_bitmap_container> container;
+			if (trace_dropped_frames_)
+			{
+				printf("image::device \"%s\" dropping input frame\n", name_.c_str());
+			}
+			shared_bitmap_container container;
 			capture_queue_.try_pop(container);
-			dropped_frames_++;
+			++dropped_frames_;
 		}
 	}
 }
 
-Handle<Value> device::get_info(void)
+Handle<Value> device::get_info() const
 {
 	HandleScope scope;
 
 	Handle<Object> o = Object::New();
-	o->Set(String::NewSymbol("dropped_frames"), convert::UInt32ToJS(dropped_frames_));
+	o->Set(String::NewSymbol("dropped_frames"), v8pp::to_v8(dropped_frames_));
 
 	return scope.Close(o);
 }
 
-} } // aspect::image
-
-
-
-#if OS(WINDOWS)
-
-// sanity check
-#pragma warning ( disable : 4702 ) // unreachable code
-BOOL APIENTRY DllMain(HANDLE hModule, DWORD  fdwReason, LPVOID)
-{
-	switch (fdwReason)
-	{
-		case DLL_PROCESS_DETACH:
-			{
-				_aspect_assert(aspect::image::g_reference_count == 0 && "image library was not terminated correctly");
-			}
-			break;
-	}
-	return TRUE;
-}
-
-#endif
-
+}} // aspect::image
--- src/image.hpp
+++ src/image.hpp
@@ -5,9 +5,7 @@
 #include "types.hpp"
 #include "geometry.hpp"
 #include "v8_core.hpp"
-#include "shared_ptr_object.hpp"
-#include "boost/thread/shared_mutex.hpp"
-// #include "math.hpp"
+#include "threads.hpp"
 
 #include <boost/thread/shared_mutex.hpp>
 
@@ -43,185 +41,166 @@ enum encoding
 //template<class _Ax = std::allocator<uint8_t>>
 class IMAGE_API bitmap : boost::noncopyable
 {
-	public:
-		explicit bitmap()
-			: pixel_format_(UNKNOWN)
+public:
+	explicit bitmap()
+		: pixel_format_(UNKNOWN)
+	{
+	}
+
+	bitmap(unsigned width, unsigned height, encoding pixel_format = BGRA8)
+	{
+		resize(width, height, pixel_format);
+	}
+
+	~bitmap()
+	{
+		total_memory_ -= data_.size();
+	}
+
+	void resize(unsigned width, unsigned height, encoding pixel_format)
+	{
+		boost::unique_lock<boost::shared_mutex> lock(shared_mutex_);
+
+		if (width == size_.width && height == size_.height && pixel_format_ == pixel_format)
 		{
+			return;
 		}
 
-		explicit bitmap(unsigned width, unsigned height, encoding pixel_format = BGRA8)
+		total_memory_ -= data_.size();
+		data_.resize(width * height * bytes_per_pixel(pixel_format), 0);
+		total_memory_ += data_.size();
+
+		size_.width = width;
+		size_.height = height;
+		pixel_format_ = pixel_format;
+	}
+
+	image_size size() const { return size_; } 
+	uint32_t width() const { return size_.width; }
+	uint32_t height() const { return size_.height; }
+	encoding pixel_format() const { return pixel_format_; }
+	uint32_t bytes_per_pixel() const { return bytes_per_pixel(pixel_format_); }
+
+	static uint32_t bytes_per_pixel(encoding pixel_format)
+	{
+		switch (pixel_format)
 		{
-			resize(width, height,pixel_format);
+			case YUV8: return 2;
+			case A8:   return 1;
+			case RGB8: return 3;
+			default:   return 4; // all other formats use 4 bytes per pixel
 		}
+	}
 
-		~bitmap()
-		{
-			total_memory_ -= data_.size();
-		}
+	uint32_t stride() const { return width() * bytes_per_pixel(); }
 
-		void resize(unsigned width, unsigned height, encoding pixel_format)
-		{
-			boost::unique_lock<boost::shared_mutex> lock(shared_mutex_);
-			if(width == size_.width && height == size_.height && pixel_format_ == pixel_format)
-				return;
-			//		boost::lock_guard<boost::shared_mutex> lock(shared_mutex_);
-			total_memory_ -= data_.size();
+	uint8_t const* data() const { return data_.empty()? nullptr : &data_[0]; }
+	uint8_t* data() { return data_.empty()? nullptr : &data_[0]; }
 
-			pixel_format_ = pixel_format;
-			data_.resize(width * height * get_bytes_per_pixel());
-			total_memory_ += data_.size();
-			size_.width = width;
-			size_.height = height;
-			memset(data(),0,data_size());
-		} 
+	size_t data_size() const { return data_.size(); }
 
-		image_size size() const { return size_; } 
-		uint32_t width() const { return size_.width; }
-		uint32_t height() const { return size_.height; }
-		encoding get_pixel_format() const { return pixel_format_; }
-		uint32_t get_bytes_per_pixel() const 
-		{  
-			switch(pixel_format_)
-			{
-				case YUV8: return 2;
-				case A8:		return 1;
-				case RGB8:		return 3;
-				default:			return 4; // all other formats use 4 bytes per pixel
-			}
-		}
-		uint32_t get_row_bytes() const { return width() * get_bytes_per_pixel(); }
+	boost::shared_mutex& shared_mutex() { return shared_mutex_; }
 
-		uint8_t const* data() const { return data_.empty()? NULL : &data_[0]; }
-		uint8_t* data() { return data_.empty()? NULL : &data_[0]; }
+	/// Create checkerboard with lines between checkers
+	void checker3(const uint32_t c1 = 0x20202020, const uint32_t c2 = 0x40404040, const uint32_t c3 = 0x80808080);
 
-		size_t data_size() const { return data_.size(); }
+	/// Create checkerboard without lines between checkers
+	void checker2(const uint32_t c1 = 0x00000000, const uint32_t c2 = 0xffffffff);
 
-		boost::shared_mutex& shared_mutex() { return shared_mutex_; }
+private:
+	boost::shared_mutex shared_mutex_;
 
-		/// Create checkerboard with lines between checkers
-		void checker3(const uint32_t c1 = 0x20202020, const uint32_t c2 = 0x40404040, const uint32_t c3 = 0x80808080);
+	image_size size_;
+	std::vector<uint8_t, aligned_allocator<uint8_t, 32>> data_;
+	encoding pixel_format_;
 
-		/// Create checkerboard without lines between checkers
-		void checker2(const uint32_t c1 = 0x00000000, const uint32_t c2 = 0xffffffff);
-
-
-	private:
-		boost::shared_mutex shared_mutex_;	
-
-		image_size size_;
-		//	buffer data_;
-		std::vector<uint8_t, aligned_allocator<uint8_t, 32>> data_;
-		//	std::vector<uint8_t, _Ax> data_;
-		encoding	pixel_format_;
-
-		static uint64_t total_memory_;
+	static uint64_t total_memory_;
 };
 
 typedef boost::shared_ptr<bitmap> shared_bitmap;
 
-
 class IMAGE_API shared_bitmap_container
 {
-	public:
+public:
 
-		enum flags {
-			DEFAULT	= 0x00000000,		// uninitialized
-			INPUT	= 0x00000001,		// belongs to input devices
-			OUTPUT	= 0x00000002,		// belongs to output devices
-			LOCAL	= 0x00000004		// for local use
-		};
+	enum flags {
+		DEFAULT	= 0x00000000,		// uninitialized
+		INPUT	= 0x00000001,		// belongs to input devices
+		OUTPUT	= 0x00000002,		// belongs to output devices
+		LOCAL	= 0x00000004		// for local use
+	};
 
-		shared_bitmap_container()
-			: flags_(DEFAULT)
-		{ }
+	shared_bitmap_container()
+		: flags_(DEFAULT)
+	{
+	}
 
-		// 		shared_bitmap_container(bitmap* color)
-		// 			: color_(shared_bitmap(color))
-		// 		{ }
-		// 
-		shared_bitmap_container(shared_bitmap& color, uint32_t flags)
-			: color_(color), flags_(flags)
-		{ }
+	shared_bitmap_container(shared_bitmap color, uint32_t flags)
+		: color_(color)
+		, flags_(flags)
+	{
+	}
 
-		// 		shared_bitmap_container(bitmap* color, bitmap* alpha)
-		// 			: color_(sha), alpha_(alpha)
-		// 		{ }
-		// 
-		shared_bitmap_container(shared_bitmap& color, shared_bitmap& alpha, uint32_t flags)
-			: color_(color), alpha_(alpha), flags_(flags)
-		{ }
+	shared_bitmap_container(shared_bitmap color, shared_bitmap alpha, uint32_t flags)
+		: color_(color)
+		, alpha_(alpha)
+		, flags_(flags)
+	{
+	}
 
-		void set_color(shared_bitmap& color) { color_ = color; }
-		shared_bitmap& get_color(void) { return color_; }
-		void set_alpha(shared_bitmap& alpha) { alpha_ = alpha; }
-		shared_bitmap& get_alpha(void) { return alpha_; }
-		bool has_alpha(void) const { return alpha_.get() ? true : false; }
-		uint32_t get_flags(void) const { return flags_; }
-		void set_flags(uint32_t flags) { flags_ = flags; }
+	void set_color(shared_bitmap color) { color_ = color; }
+	shared_bitmap color() const { return color_; }
 
-	private:
+	void set_alpha(shared_bitmap alpha) { alpha_ = alpha; }
+	shared_bitmap alpha() const { return alpha_; }
 
-		shared_bitmap	color_;	// source data
-		shared_bitmap	alpha_;	// separate alpha (if available)
-		//		shared_bitmap	aux_;	// auxiliary (converted 
-		uint32_t		flags_;
+	uint32_t flags() const { return flags_; }
+	void set_flags(uint32_t flags) { flags_ = flags; }
+
+private:
+	shared_bitmap color_;	// source data
+	shared_bitmap alpha_;	// separate alpha (if available)
+	uint32_t      flags_;
 };
 
-class IMAGE_API device : public shared_ptr_object<device>
+class IMAGE_API device
 {
-	public:
+public:
+	explicit device(char const* name = "N/A")
+		: name_(name? name : "")
+		, encoding_(UNKNOWN)
+		, dropped_frames_(0)
+		, trace_dropped_frames_(false)
+	{
+	}
 
-		V8_DECLARE_CLASS_BINDER(device);
+	virtual ~device()
+	{
+	}
 
-		device(const char *name = "N/A")
-			: name_(name), encoding_(UNKNOWN), dropped_frames_(0), trace_dropped_frames_(false)
-		{
-		}
+	encoding get_encoding() const { return encoding_; }
+	void set_encoding(encoding encoding) { encoding_ = encoding; }
 
-		virtual ~device()
-		{
-		}
+	uint32_t get_dropped_frames() const { return dropped_frames_; }
 
-		virtual bool acquire_input_frame(boost::shared_ptr<shared_bitmap_container> & frame) { return capture_queue_.try_pop(frame); }
-		virtual void acquire_input_frame_blocking(boost::shared_ptr<shared_bitmap_container> & frame) { return capture_queue_.wait_and_pop(frame); }
-		virtual void release_input_frame(boost::shared_ptr<shared_bitmap_container> & frame) { available_queue_.push(frame); }
-		virtual void schedule_output_frame(boost::shared_ptr<shared_bitmap_container> & ) { _aspect_assert(false && "aspecet::image::device::schedule_output_frame() is not overloaded"); }
-		virtual void schedule_input_frame(boost::shared_ptr<shared_bitmap_container> & frame, bool drop_frames);
+	virtual v8::Handle<v8::Value> get_info() const;
 
-		encoding get_encoding(void) const { return encoding_; }
-		void set_encoding(encoding encoding) { encoding_ = encoding; }
+	virtual bool acquire_input_frame(shared_bitmap_container& frame) { return capture_queue_.try_pop(frame); }
+	virtual void acquire_input_frame_blocking(shared_bitmap_container& frame) { return capture_queue_.wait_and_pop(frame); }
+	virtual void release_input_frame(shared_bitmap_container const& frame) { available_queue_.push(frame); }
+	virtual void schedule_output_frame(shared_bitmap_container) { _aspect_assert(false && "aspecet::image::device::schedule_output_frame() is not overloaded"); }
+	virtual void schedule_input_frame(shared_bitmap_container const& frame, bool drop_frames);
 
-		//		threads::concurrent_queue<boost::shared_ptr<bitmap>>& get_capture_queue(void) { return capture_queue_; }
-		threads::concurrent_queue<boost::shared_ptr<shared_bitmap_container>>& get_capture_queue(void) { return capture_queue_; }
+private:
+	std::string name_;
+	encoding encoding_;
+	uint32_t dropped_frames_;
+	bool trace_dropped_frames_;
 
-		uint32_t get_dropped_frames(void) { return dropped_frames_; }
-
-		virtual v8::Handle<v8::Value> get_info(void);
-
-	protected:
-
-		std::string name_;
-
-		threads::concurrent_queue<boost::shared_ptr<shared_bitmap_container>>	capture_queue_;
-		threads::concurrent_queue<boost::shared_ptr<shared_bitmap_container>>	available_queue_;
-
-		encoding encoding_;
-
-		uint32_t dropped_frames_;
-		bool trace_dropped_frames_;
+	threads::concurrent_queue<shared_bitmap_container> capture_queue_;
+	threads::concurrent_queue<shared_bitmap_container> available_queue_;
 };
 
-// NOTE - this library must be initialized and cleaned up using these functions!
-void IMAGE_API init(void);
-void IMAGE_API cleanup(void);
-
-extern IMAGE_API std::vector<boost::shared_ptr<device>>	g_devices;
-
-} } // aspect::image
-
-
-#define WEAK_CLASS_TYPE aspect::image::device
-#define WEAK_CLASS_NAME __image_device_interface
-#include <v8/juice/WeakJSClassCreator-Decl.h>
+}} // aspect::image
 
 #endif // IMAGE_HPP_INCLUDED
